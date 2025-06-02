5ce4a45e8b7fe1ea1c70b1b052580c6454850529
lavanyat24 <lavanyat443@gmail.com>
2025-06-02 23:05:03 +0530

    add to github

---
+++ Task-1/script.js
@@ -0,0 +1,12 @@
+
+ document.getElementById("shoreTemple").addEventListener("click", function() {
+    alert("Description: A 7th-century temple overlooking the Bay of Bengal. UNESCO World Heritage Site.");
+});
+
+document.getElementById("MahabalipuramBeach").addEventListener("click", function() {
+    alert("Description: Scenic beach ideal for walking, relaxing, or watching the sunrise near ancient temples.");
+});
+
+document.getElementById("PanchaRathas").addEventListener("click", function() {
+    alert("Description: Monolithic rock-cut temples shaped like chariots, each carved from a single granite stone.");
+});
