76df961f835115d0830931fafdc45a36e53246d9
lavanyat24 <lavanyat443@gmail.com>
2025-06-03 13:19:29 +0530

    responsive add

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,12 +1,11 @@
-
- document.getElementById("shoreTemple").addEventListener("click", function() {
-    alert("Description: A 7th-century temple overlooking the Bay of Bengal. UNESCO World Heritage Site.");
+document.getElementById("shoreTemple").addEventListener("click", function () {
+  alert("Shore Temple:\nA 7th-century temple overlooking the Bay of Bengal. UNESCO World Heritage Site.");
 });
 
-document.getElementById("MahabalipuramBeach").addEventListener("click", function() {
-    alert("Description: Scenic beach ideal for walking, relaxing, or watching the sunrise near ancient temples.");
+document.getElementById("MahabalipuramBeach").addEventListener("click", function () {
+  alert("Mahabalipuram Beach:\nScenic beach ideal for walking, relaxing, or watching the sunrise near ancient temples.");
 });
 
-document.getElementById("PanchaRathas").addEventListener("click", function() {
-    alert("Description: Monolithic rock-cut temples shaped like chariots, each carved from a single granite stone.");
+document.getElementById("PanchaRathas").addEventListener("click", function () {
+  alert("Pancha Rathas:\nMonolithic rock-cut temples shaped like chariots, each carved from a single granite stone.");
 });
